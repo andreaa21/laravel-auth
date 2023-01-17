18ef883da9330e5e65b2a3adf2855c20032477d1
andreaa21 <andrea.zollini@proton.me>
2023-01-17 18:00:30 +0100

    add guest

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class DashboardController extends Controller
+class PageController extends Controller
 {
     public function index()
     {
-        return view('admin.home');
+        return view('guest.home');
     }
 }
