40733dd72a435f21a93a9dd16d65c6003e2dd1b6
andreaa21 <andrea.zollini@proton.me>
2023-01-18 17:23:12 +0100

    add table to index

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -11,6 +11,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        return view('admin.projects.index');
+        $projects = Project::all();
+        return view('admin.projects.index', compact('projects'));
     }
 }
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -5,8 +5,25 @@
         <h1 class="mt-3">Elenco dei progetti</h1>
 
         <div class="row">
-            <div class="col">
-
+            <div class="col mt-5">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Client Name</th>
+                            <th scope="col">Summary</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($projects as $project)
+                            <tr>
+                                <th scope="row">{{ $project->name }}</th>
+                                <td>{{ $project->client_name }}</td>
+                                <td>{{ $project->summary }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
