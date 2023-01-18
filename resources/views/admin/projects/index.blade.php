4d559c32fe41e22ea117f9a0bd8fa2882df928de
andreaa21 <andrea.zollini@proton.me>
2023-01-18 17:17:07 +0100

    add index

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1 class="mt-3">Elenco dei progetti</h1>
+
+        <div class="row">
+            <div class="col">
+
+            </div>
+        </div>
+    </div>
+@endsection
