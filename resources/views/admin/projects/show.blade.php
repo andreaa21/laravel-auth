6d86f4d0bd5031c235191d2ca060cf1b618eb9d3
andreaa21 <andrea.zollini@proton.me>
2023-01-20 14:59:42 +0100

    add show view

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
     <div class="container">
-        <h1>show</h1>
+        <div class="row">
+            <div class="col card mt-5">
+                <h5 class="card-title my-2">{{ $project->name }}</h5>
+                <h5 class="card-title my-2">{{ $project->client_name }}</h5>
+                <h5 class="card-title my-2">{{ $project->summary }}</h5>
+                <img width="500" class="my-5" src="{{ $project->cover_image }}" alt="{{ $project->name }}">
+            </div>
+        </div>
+        <a class="btn btn-primary my-4" href=" {{ route('admin.projects.index') }} ">INDIETRO </a>
     </div>
 @endsection
